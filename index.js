d438cb2b74fa4d866fcb7446caa3ad4618b8c7ae
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2021-05-14 19:16:40 +0200

    Use Superstucture

---
+++ index.js
@@ -0,0 +1,33 @@
+// Require superstructure
+const superstructure = require("superstructure");
+
+// Set your configuration
+const config = {
+	// Name of your website
+	name: 'Spaceships in Space',
+	// What your website is about
+	description: "Gaeel Bradshaw spaceship",
+	// Canonical URL for your website
+	siteUrl: 'https://spaceshipsin.space/',
+	// Where to put the generated website
+	dest: './build',
+	// Where all your content is
+	root: './sources',
+	// Files to copy verbatim
+	public: 'public',
+	// Page templates (must include a layout.pug template at least)
+	templates: 'pug',
+	// Stylesheets (written in sass)
+	css: 'sass/out',
+	// Your articles (written in Markdown)
+	articles: 'markdown/out',
+	// Images types to make compressed versions of 
+	crunch: [".jpg",".png"],
+};
+
+
+superstructure.build(config)
+	.then(()=>{
+		// Code you want to run when the build is done
+		console.log("Launched!");
+	});
